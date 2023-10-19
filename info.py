a5766a107680d8e60bc8a72d07e84da987224e25
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-20 00:21:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '28483301'))
-API_HASH = environ.get('API_HASH', '187b27cc4f74d20b717c0563aade821d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6239835382:AAH-YcVJ8_eBypHta4hFqLMcfYI4RSS1mvs")
+API_ID = int(environ.get('API_ID', '23762978'))
+API_HASH = environ.get('API_HASH', 'eff30dac5504a8660e69bfe19f668571')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6325916844:AAEQ8XTUudQt5z20-lZ7aJ-WQBL_1LilFTw")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -29,17 +29,17 @@ VRFIED_IMG = environ.get("VRFIED_IMG", "https://telegra.ph/file/5f1a3df8d7029f29
 VRFY_IMG = environ.get("VRFY_IMG", "https://telegra.ph/file/5f1a3df8d7029f29287a0.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5318559054').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6683865620').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001896066430').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '0').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6683865620').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001688046237')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001924425523')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
@@ -52,18 +52,18 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'MdiskShortner.link')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e5f45a53be010affc718b6fffbc0004a6cf4a6eb')
+SHORTLINK_API = environ.get('SHORTLINK_API', '00b0eacdca74780666d78f22ec43440e43994c2b')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/netflix007rq')
-SPRT_CHNL = environ.get('SPRT_CHNL', 'https://t.me/CinemaV00')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+CGTTz-Sa6VRiZWZl')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/CinemaV00/299')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Cinemavilla0012')
+SPRT_CHNL = environ.get('SPRT_CHNL', 'https://t.me/netflixvilla002')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/netflixvilla002')
+TUTORIAL = environ.get('TUTORIAL', '')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/CinemaV00/299')
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', '')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001940288450'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
