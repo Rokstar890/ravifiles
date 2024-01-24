160cb942c1fbf24185725d8a0558b2a1c8673f06
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-25 00:50:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,11 +29,11 @@ VRFIED_IMG = environ.get("VRFIED_IMG", "https://telegra.ph/file/5f1a3df8d7029f29
 VRFY_IMG = environ.get("VRFY_IMG", "https://telegra.ph/file/5f1a3df8d7029f29287a0.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6683865620').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5318559054').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001896066430').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '0').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6683865620').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
